6579381fcc0e0cb6ada4508452778cb79d79165c
Yann <yannguimaraes5@outlook.com>
2024-01-22 18:33:28 -0300

    First commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,63 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timeId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime     //textContent é pra alterar o conteudo de texto do elemento.
+    if (state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timeId);
+        alert(`Game Over! O seu resultado foi: ${state.values.result}`)
+    }
+}
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.05;
+    audio.play();
+}
+
+function randomSquare(){//percorrer todas as squares e verificar se tem o "enemy", se tiver, irá excluir.
+    state.view.squares.forEach((square) =>{
+        square.classList.remove("enemy");
+    })
+    let randomNumber = Math.floor(Math.random()*9);
+    let randomSquare =  state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox(){ //o Listener espera uma ação pra ser executada. Ele fica "ouvindo".
+    state.view.squares.forEach((square)=> {
+        square.addEventListener("mousedown", () => {
+           if (square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+           }
+        })           //addEventListener adiciona a ação ao clicar.
+    })
+
+}
+
+function init() {
+    addListenerHitBox();
+}
+
+init();
